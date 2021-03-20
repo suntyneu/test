81b0272a5cadf685e90bff9ac46350d075a4177f
suntyneu <19904270266@189.cn>
2021-03-20 14:50:19 +0800

    Default Changelist

--- qc Creator/main.py
+++ qc Creator/main.py
@@ -1,11 +1,11 @@
 import sys
-import MainWinsingelSlot
+import dem1
 from PyQt5.QtWidgets import QApplication, QMainWindow
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)  # 创建QApplication 代表整个应用程序 固定写法
     mainWindow = QMainWindow()  # 创建 mainWindow对象
-    ui = MainWinsingelSlot.Ui_MainWindow()  # 创建Ui_MainWindow类实例化
+    ui = dem1.Ui_MainWindow()  # 创建Ui_MainWindow类实例化
     ui.setupUi(mainWindow)     # 向主窗口添加控件
     mainWindow.show()
     sys.exit(app.exec_())      # 进入主循环
